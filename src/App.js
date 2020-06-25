d1549021221985521c834c235fee6c060b3f57f7
Ukpa Uchechi <ukpauchechi1@gmail.com>
2020-06-25 10:43:03 +0100

    frontend changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import GetFiles from './Endpiont/GetFiles';
+import GetOneFile from './Endpiont/GetOneFile';
+import Createfile from './Endpiont/Createfile';
+import Editfile from './Endpiont/Editfile';
+import Deletefile from './Endpiont/Deletefile';
+import HomePage from './Home/HomePage';
+import Division from './BodyDivision/Division';
+import Blog from './Blog/Blog';
+import Portfolio from './Portfolio/Portfolio';
+import NotFound from './Notfound/NotFound';
+import Search from './search/Search';
+import Project from './Portfolio/Project';
+import Mainport from './Portfolio/Mainport';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      < Router >
+        <div className='App'>
+          <Switch>
+          <Route path='/' exact component={HomePage} />
+          <Route path='/createfile' exact component={Createfile} />
+          <Route path='/getfile/:id' exact component={GetOneFile} />
+          <Route path='/editfile/:id' exact component={Editfile} />
+          <Route path='/deletefile/:id' exact component={Deletefile} />
+          {/* <Route path='/homepage' exact component={HomePage} /> */}
+          <Route path='/started' exact component={Division} />
+          <Route path='/blog' exact component={Blog} />
+          <Route path='/portfolio' exact component={Portfolio} />
+          <Route path='/create' exact component={Search} />
+          <Route path='/project' exact component={Project} />
+          <Route path='/main' exact component={Mainport} />
+          <Route component={NotFound} />
+
+          </Switch>
+        </div>
+
+      </Router >
+    
+    // <div>
+    //   <Createfile />
+    // </div>
+    
   );
 }
 
